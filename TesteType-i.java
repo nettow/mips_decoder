c32e3ee7578e880762a388ee3a723b1677aed95e
João Silveira <jlsnsilveira@sga.pucminas.br>
2020-03-20 22:39:58 -0300

    Teste do type i

--- TesteType-i.java
+++ TesteType-i.java
@@ -1,12 +1,14 @@
 class Teste {
 
     public static void main(String[] args) {
-        String opCode, immediate;
-        int IntegerImmediate, rsNumber, rtNumber;
-        String rs = "null";
-        String rt = "null";
-        String instruction = "andi $s2,$s3,4";
-        String binaryResult = "null";
+        String opCode, immediate = "";
+        int integerImmediate, rsDecimal, rtDecimal;
+        String rs = ""; 
+        String rt = ""; 
+        String baseReg = "";
+        String instruction = "lw $s1,4($t1)";
+        String binaryResult;
+
 
         String[] divInstruction = instruction.split(" ");
         opCode = OpCodeTable.getOpCode(divInstruction[0]);
@@ -16,21 +18,26 @@ class Teste {
         // newInstructions[] = [$s2,$s4,4]
 
         if (opCode == "000011" || opCode == "001010" || opCode == "001100" || opCode == "001101") { // ADDI, SLTI, ANDI, ORI
-            rtNumber = RegistersBinaryTable.getRegBinaryValue(newInstructions[0]);
-            rt = Integer.toBinaryString(rtNumber);
+            rtDecimal = RegistersDecimalTable.getRegDecimalValue(newInstructions[0]);
+            rt = Integer.toBinaryString(rtDecimal);
 
-            rsNumber = RegistersBinaryTable.getRegBinaryValue(newInstructions[1]);
-            rs = Integer.toBinaryString(rsNumber);
+            rsDecimal = RegistersDecimalTable.getRegDecimalValue(newInstructions[1]);
+            rs = Integer.toBinaryString(rsDecimal);
 
-            IntegerImmediate = Integer.parseInt(newInstructions[2]);
-            immediate = completeSixteenBits(IntegerImmediate);
+            integerImmediate = Integer.parseInt(newInstructions[2]);
+            immediate = completeSixteenBits(integerImmediate);
 
             binaryResult = "opcode: "+ opCode +"\nrt: " + rt + "\nrs:" + rs +"\nimmediate: "+ immediate;
             System.out.println(binaryResult);
         } else if (opCode == "100011" || opCode == "101011") { // LW and SW
-            rtNumber = RegistersBinaryTable.getRegBinaryValue(newInstructions[0]);
-            rt = Integer.toBinaryString(rtNumber);
-            binaryResult = "NADA POR ENQUANTO";
+            rtDecimal = RegistersDecimalTable.getRegDecimalValue(newInstructions[0]);
+            rt = Integer.toBinaryString(rtDecimal);
+            baseReg = newInstructions[1].substring(newInstructions[1].indexOf("(") + 1, newInstructions[1].indexOf(")") );
+            RegistersDecimalTable.getRegDecimalValue(baseReg);
+            integerImmediate = rtDecimal + Integer.parseInt(baseReg);
+            immediate = completeSixteenBits(integerImmediate);
+
+            binaryResult = opCode + rt + rs + immediate;
             System.out.println(binaryResult);
         }else if (opCode == "000100" || opCode == "000101"){ // BEQ and BNE
 
