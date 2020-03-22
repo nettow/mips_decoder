3745c3cb81a0fcbd78e01016211142986edc5275
João Silveira <jlsnsilveira@sga.pucminas.br>
2020-03-22 00:25:03 -0300

    little change

--- Teste.java
+++ Teste.java
@@ -2,7 +2,7 @@ import decoders.*;
 public class Teste{
 
     public static void main(String[] args){
-        String result = DecoderTypeI.proccessInstructionTypeI("addi $s1, $s2, 2");
+        String result = RegisterDecoder.decode("addi $s1,$s2, 2");
         System.out.println(result);
     }
 }
--- decoders/DecoderTypeI.java
+++ decoders/DecoderTypeI.java
@@ -3,23 +3,19 @@ import tables.*;
 public class DecoderTypeI {
     // TYPE I [OpCode, rs, rt, immediate (16 bits)]
     public static String proccessInstructionTypeI(String instruction) {
-        String opCode, immediate = "";
-        int integerImmediate, rsDecimal, rtDecimal;
+        String immediate = "";
         String rs = "";
         String rt = "";
         String baseReg = "";
-
         String binaryResult;
+        int integerImmediate, rsDecimal, rtDecimal;
 
-        String divInstruction = instruction.substring(0, instruction.indexOf(" "));
-        opCode = OpCodeTable.getOpCode(divInstruction);
-        // divInstruction[1] = resto (ex: $s0,$s1,4)
+        String opCode = OpCodeTable.getOpCode(instruction.substring(0, instruction.indexOf(" ")));
 
-        String newInstructionsStr = instruction.substring(instruction.indexOf(" "), instruction.length() ).replace(" ", "");
+        String newInstructionsStr = instruction.substring(instruction.indexOf(" "), instruction.length()).replace(" ","");
         String[] newInstructions = newInstructionsStr.split(",");
-        // newInstructions[] = [$s0,$s1,4]
 
-        if (opCode == "000011" || opCode == "001010" || opCode == "001100" || opCode == "001101") { // ADDI, SLTI, ANDI
+        if (opCode == "000011" || opCode == "001010" || opCode == "001100" || opCode == "001101") { // ADDI, SLTI, ANDI, ORI
             rtDecimal = RegistersDecimalTable.getRegDecimalValue(newInstructions[0]);
             rt = Integer.toBinaryString(rtDecimal);
 
