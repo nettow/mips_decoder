bcbaaeb3c6485e88152a9cd4664f145d60d91cf3
Joao Silveira <jlsnsilveira@sga.pucminas.br>
2020-03-25 04:23:47 -0300

    update Teste.java

--- Teste.java
+++ Teste.java
@@ -7,13 +7,27 @@ public class Teste{
         System.out.println("lw $t2,10($s1): " + RegisterDecoder.decode(("lw $t2,10($s1)")));
         System.out.println("sw $t2,10($s1): " + RegisterDecoder.decode(("sw $s1,14($t2)")));
         System.out.println("andi $s1,$s2,4: " + RegisterDecoder.decode(("andi $s1,$s2,4")));
+        System.out.println("BEQ: depende do PC");
+        System.out.println("BNE: depende do PC");
+        System.out.println("ori: ");
+        System.out.println("slti: ");
         System.out.println();
         System.out.println("TYPE R \n");
         System.out.println("add $s4,$s0,$t3: " + RegisterDecoder.decode(("add $s4,$s0,$t3")));
         System.out.println("sub: $s4,$s0,$t3" + RegisterDecoder.decode(("sub $s4,$s0,$t3")));
-        //System.out.println("mult: $s4,$s0" + RegisterDecoder.decode(("sub $s4,$s1")));
+        System.out.println("and: ");
+        System.out.println("or: ");
+        System.out.println("xor: ");
+        System.out.println("sll: ");
+        System.out.println("slr: ");
+        System.out.println("mult: ");
+        System.out.println("div: ");
+        System.out.println("neg: ");
 
         System.out.println();
         System.out.println("TYPE J \n");
+        System.out.println("J: depende do PC");
+        System.out.println("JAL: depende do PC");
+        System.out.println("JR: depende do PC");
     }
 }
