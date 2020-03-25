49e071fcf35e5c36b5934b893a41875ad2c376e4
Joao Silveira <jlsnsilveira@sga.pucminas.br>
2020-03-25 04:07:08 -0300

    update Teste.java

--- Teste.java
+++ Teste.java
@@ -2,7 +2,18 @@ import decoders.*;
 public class Teste{
 
     public static void main(String[] args){
-        String result = RegisterDecoder.decode("addi $s1,$s2, 2");
-        System.out.println(result);
+        System.out.println("\nTYPE I \n");
+        System.out.println("addi $t3,$s2,20: " + RegisterDecoder.decode(("addi $t3,$s2,20")));
+        System.out.println("lw $t2,10($s1): " + RegisterDecoder.decode(("lw $t2,10($s1)")));
+        System.out.println("sw $t2,10($s1): " + RegisterDecoder.decode(("sw $s1,14($t2)")));
+        System.out.println("andi $s1,$s2,4: " + RegisterDecoder.decode(("andi $s1,$s2,4")));
+        System.out.println();
+        System.out.println("TYPE R \n");
+        System.out.println("add $s4,$s0,$t3: " + RegisterDecoder.decode(("add $s4,$s0,$t3")));
+        System.out.println("sub: $s4,$s0,$t3" + RegisterDecoder.decode(("sub $s4,$s0,$t3")));
+        //System.out.println("mult: $s4,$s0" + RegisterDecoder.decode(("sub $s4,$s1")));
+
+        System.out.println();
+        System.out.println("TYPE J \n");
     }
 }
