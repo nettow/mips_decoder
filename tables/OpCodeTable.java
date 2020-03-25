7dd894e4f2f09c6f0813a89e267df9f2b83f824a
Joao Silveira <jlsnsilveira@sga.pucminas.br>
2020-03-25 04:06:04 -0300

    little change

--- tables/OpCodeTable.java
+++ tables/OpCodeTable.java
@@ -9,13 +9,13 @@ public class OpCodeTable{
             case "jal": return "000011";
             //type I
             case "addi": return "000011";
-            case "lw": return "100011"; // R[rt] = M[R[rs]+SignExtImm] 
-            case "sw": return "101011"; // M[R[rs]+SignExtImm] = R[rt]
-            case "beq": return "000100"; // if(R[rs]==R[rt]) -> PC=PC+4+BranchAddr
-            case "bne": return "000101"; // if(R[rs]!=R[rt]) -> PC=PC+4+BranchAddr
+            case "lw": return "100011";
+            case "sw": return "101011";
+            case "beq": return "000100";
+            case "bne": return "000101";
             case "slti": return "001010";
-            case "andi": return "001100"; 
-            case "ori": return "001101"; 
+            case "andi": return "001100";
+            case "ori": return "001101";
         }
 
         return null;
@@ -36,7 +36,7 @@ public class OpCodeTable{
             case "srl": return "000010";
             case "jr": return "001000";
             case "or": return "100101";
-            //case "neg": return ""
+            case "neg": return "000111";
         }
 
         return null;
