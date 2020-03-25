704f39974efec63f631148170ef63fc66b84f52a
Joao Silveira <jlsnsilveira@sga.pucminas.br>
2020-03-24 22:05:43 -0300

    Comentarios

--- decoders/DecoderTypeI.java
+++ decoders/DecoderTypeI.java
@@ -16,14 +16,14 @@ public class DecoderTypeI {
         String[] newInstructions = newInstructionsStr.split(",");
 
         if (opCode == "000011" || opCode == "001010" || opCode == "001100" || opCode == "001101") { // ADDI, SLTI, ANDI, ORI
-            rtDecimal = RegistersDecimalTable.getRegDecimalValue(newInstructions[0]);
-            rt = Integer.toBinaryString(rtDecimal);
+            rtDecimal = RegistersDecimalTable.getRegDecimalValue(newInstructions[0]); // rtDecimal recebe o valor em decimal do registrador em newInstruction[0]
+            rt = Integer.toBinaryString(rtDecimal); // rt recebe rtDecimal transformado em binario
 
-            rsDecimal = RegistersDecimalTable.getRegDecimalValue(newInstructions[1]);
-            rs = Integer.toBinaryString(rsDecimal);
+            rsDecimal = RegistersDecimalTable.getRegDecimalValue(newInstructions[1]); // rsDecimal recebe o valor em decimal do registrador em newInstruction[1]
+            rs = Integer.toBinaryString(rsDecimal); // rrs recebe rsDecimal transformado em binario
 
-            integerImmediate = Integer.parseInt(newInstructions[2]);
-            immediate = RegisterDecoder.completeBits(integerImmediate,16);
+            integerImmediate = Integer.parseInt(newInstructions[2]); // // integerImmediate recebe o valor da constante transformado para inteiro
+            immediate = RegisterDecoder.completeBits(integerImmediate,16); // completa os bits faltantes pro immediate ter 16 bits
 
         } else if (opCode == "100011" || opCode == "101011") { // LW and SW
             rtDecimal = RegistersDecimalTable.getRegDecimalValue(newInstructions[0]);
@@ -33,7 +33,7 @@ public class DecoderTypeI {
             immediate = RegisterDecoder.completeBits(integerImmediate,16);
 
         } else if (opCode == "000100" || opCode == "000101") { // BEQ and BNE
-
+            
         }
 
         binaryResult = opCode + rt + rs + immediate;
