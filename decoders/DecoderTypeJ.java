614b8a8bef2f28bff00af12b55a25e3492d016c4
nettow <jlsnsilveira@sga.pucminas.br>
2020-03-26 02:23:51 -0300

    Type J completo

--- decoders/DecoderTypeJ.java
+++ decoders/DecoderTypeJ.java
@@ -2,18 +2,11 @@ package decoders;
 import tables.*;
 public class DecoderTypeJ{
     public static String proccessInstructionTypeJ(String instruction) {
-        String opCode;
-        String address = "";
-        int integerAddress;
-        String binaryResult;
+        String[] dividedInstruction = instruction.split(" ");
 
-        String[] divInstruction = instruction.split(" ");
-        opCode = OpCodeTable.getOpCode(divInstruction[0]);
-        if (opCode == "000010" || opCode == "000011"){ // j and jal
-            integerAddress = Integer.parseInt(divInstruction[1]);
-            address = RegisterDecoder.completeBits(integerAddress,26);
-        }
-        binaryResult = opCode + address;
-        return binaryResult;
+        String opCode = OpCodeTable.getOpCode(dividedInstruction[0]);
+        String address = RegisterDecoder.completeBits(Integer.parseInt(dividedInstruction[1]), 25);
+
+        return opCode + address;
     }
 }
