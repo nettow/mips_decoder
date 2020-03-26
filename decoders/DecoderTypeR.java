8b8d41553806d85f85d747c1dd4d749c471732ef
nettow <jlsnsilveira@sga.pucminas.br>
2020-03-26 02:05:11 -0300

    comandos sll e slr fixados

--- decoders/DecoderTypeR.java
+++ decoders/DecoderTypeR.java
@@ -13,14 +13,20 @@ public class DecoderTypeR {
 
         if (inst.equals("mult") || inst.equals("div")) {
 
-            rt = RegisterBinaryTable.getRegBinaryValue(instRegisters[0]);
-            rs = RegisterBinaryTable.getRegBinaryValue(instRegisters[1]);
+            rs = RegisterBinaryTable.getRegBinaryValue(instRegisters[0]);
+            rt = RegisterBinaryTable.getRegBinaryValue(instRegisters[1]);
 
             rd = "00000";
             shamt = "00000";
             funct = OpCodeTable.getFunct(inst);
 
-        } else {
+        } else if (inst.equals("sll") || (inst.equals("srl"))){
+            rs = "00000";
+            rt = RegisterBinaryTable.getRegBinaryValue(instRegisters[1]);
+            rd = RegisterBinaryTable.getRegBinaryValue(instRegisters[0]);
+            shamt = RegisterDecoder.completeBits(Integer.parseInt(instRegisters[2]), 5);
+            funct = OpCodeTable.getFunct(inst);
+        } else{
             rt = RegisterBinaryTable.getRegBinaryValue(instRegisters[2]);
             rs = RegisterBinaryTable.getRegBinaryValue(instRegisters[1]);
 
